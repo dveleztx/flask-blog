11dbad6697488784b768d73b685bd87323770797
dveleztx <dveleztx@gmail.com>
2019-03-05 01:06:48 -0600

    Updated blog with User and Post classes for SQL -DV

--- blog.py
+++ blog.py
@@ -1,4 +1,5 @@
 # Imports
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
@@ -8,7 +9,37 @@ app = Flask(__name__)
 
 # App Configurations
 app.config["SECRET_KEY"] = "ded04618e70e37ff38d20ac218b67ac9"
-app.config['SQLALCHEMY_DATABASE_URI'] = ""
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://fury:flaskpass@localhost/flaskblog"
+db = SQLAlchemy(app)
+
+
+def setUp(self):
+	app.db.session.commit()
+	app.db.drop_all()
+	app.db.creat_all()
+
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(20), unique=True, nullable=False)
+	email = db.Column(db.String(120), unique=True, nullable=False)
+	image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+	password = db.Column(db.String(60), nullable=False)
+	posts = db.relationship("Post", backref="author", lazy=True)
+
+	def __repr__(self):
+		return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(100), nullable=False)
+	date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+	content = db.Column(db.Text, nullable=False)
+	user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+	def __repr__(self):
+		return f"User('{self.title}', '{self.date_posted}')"
 
 
 # Blog Posts
